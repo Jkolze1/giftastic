fe34543582a59a95e177660d424f9c2b65fd87bd
Jacqueline Kolze <38360663+Jkolze1@users.noreply.github.com>
2018-06-20 15:48:25 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
- // Variables
+// Topics
 var topicArray= ["dogs","kittens", "birds", "pigs", "hedgehog", "goldifsh", ];
 
- // makes buttons baed on input from user
- function animalbuttonClicked(){
+ // Makes buttons baed on input from user
+function animalbuttonClicked(){
 var userInput=$('#animalInput').val();
 searchGif(userInput);
 }
 
-//searches for gifs
+
+// Searches for gifs
 function searchGif(gifName){
     $.ajax({
         url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=dc6zaTOxFJmzC&limit=10',
-        type: 'GET',
+        method: 'GET',
     })
-    .done(function(response) {
+    // After the data from the AJAX request comes back
+    .then(function(response) {
         displayGif(response);
     })
 }
 
-//makes gifs appear
+// Makes gifs appear
 $(document).ready(function(){
     for(var i=0; i< topicArray.length; i++){
         $("#animalbutton").append("<button type= 'button' onclick= 'searchGif(\"" + topicArray[i] + "\")' class='btn' value='" + topicArray[i] + "'> " + topicArray[i] + "</button>");
     }
 });
 
- //button clicked function
+ // Button clicked function
  function animalbuttonClicked(){
 var userInput=$('#animalInput').val();
 searchGif(userInput);
 }
 
-//submit button
+// Submit button
 function submitbuttonClicked() {
     var userInput = $('#animalInput').val();
-//if users adds input, then a button appears
+
     if (userInput) {
         $('#animalbutton').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
     }
 }
-//function display
+// Displays gif
 function displayGif(response){
     $('#animals').empty();
     for(var i=0; i<response.data.length; i++){
@@ -47,13 +49,12 @@ function displayGif(response){
 var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
     '" data-still=" ' + response.data[i].images.fixed_height_still.url +
     ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="gif" style= "width:250px; height:250px">';
-
-    //shows image
+// Shows image
     image = '<div class="col-md-4">' + image + "</div>";
     $('#animals').append(image);
 }
 
-//changes from still to motion with a click
+// Changes from still to motion with a click
 $('.gif').on('click', function() {
     var state = $(this).attr('data-state');
     if (state == 'still') {
@@ -66,4 +67,3 @@ $('.gif').on('click', function() {
 
 });
 }
-
